15e4be476718846a9dd883c2580cf64e4a5b4b20
Cristopher Ferrari <ferraricristopher93@gmail.com>
2023-12-07 15:46:24 +0100

    CSS, JS, HTML: Coding writing

--- js/main.js
+++ js/main.js
@@ -5,12 +5,29 @@ const { createApp } = Vue;
 
 createApp ({
     data() {
+        // My proprieties and values
         return {
             text : 'Hello Word!!!',
+            paragraphText : 'Hi everyone, this is my first vue exercise',
             immagine: 'https://cdn-media-1.freecodecamp.org/images/0*xkJgg-6HskYrQ3N7.jpeg',
             altImmagine : 'Evan You',
-            buttonContent : 'Click to Show you something...',
-            mieClassi : 'hidden'
+            buttonContent : 'Click to Show you more...',
+            //class name
+            miaClasse : 'hidden',
+            altraClasse : 'active',
+            classeColor : 'white'
+        }
+    },
+    methods: {
+        //function(if hidden item is hidden after click button will be active (and change buttons text))
+        changeDisplay() {
+            if(this.miaClasse === 'hidden'){
+                this.miaClasse = 'active'
+                this.buttonContent = 'Click to close'
+            } else {
+                this.miaClasse = 'hidden';
+                this.buttonContent = 'Click to Show you more...'
+            };
         }
     }
 }).mount('#app');
