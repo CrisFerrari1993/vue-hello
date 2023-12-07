3c43dd02666dddccee2146b97620e792850f93be
Cristopher Ferrari <ferraricristopher93@gmail.com>
2023-12-07 15:15:01 +0100

    JS: Vue createApp nesting

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//Descrizione:
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Vue Code here
+const { createApp } = Vue;
+
+createApp ({
+    data() {
+        return {
+            
+        }
+    }
+}).mount('#app');
+//Bonus:
+//Aggiungere alla pagina un’immagine, presa anch’essa da un data.
